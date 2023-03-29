8793e18179f8a89241180ffcc2caf8db3ea7c51d
Emmanuel Canto <emmanuel-canto@hotmail.com>
2023-03-29 11:17:57 -0500

    haciendo ejercicio 1.4

---
+++ courseinfo/1.4/src/index.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => <h1>{props.course}</h1>
+const Content = (props)=>{
+  return (
+    <div>
+      {props.parts.map((part) => 
+        <p>{part.name} {part.exercises}</p>
+      )}
+    </div>
+  )
+}
+const Total = (props) => <p> Exercises: {props.totales.reduce((sum, total) => sum + total.exercises, 0)} </p>
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts}/>
+      <Total totales={parts} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
